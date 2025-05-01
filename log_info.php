e8d73fb32c48e2b7ef636ea0d7f2834941213b96
AntoineDeparis <158306939+AntoineDeparis@users.noreply.github.com>
2025-05-02 01:18:46 +0200

    Update log_info.php

--- log_info.php
+++ log_info.php
@@ -25,3 +25,53 @@ function logConnectionAttempt($pseudo, $success) {
     
     error_log("Connexion utilisateur: $pseudo - Statut: $status");
 }
+
+function logRegisterAttempt($pseudo, $success) {
+    $logDir = getenv('HOME') . '/LogFiles/';
+    
+    if (!file_exists($logDir)) {
+        mkdir($logDir, 0755, true);
+    }
+    
+    $logFile = $logDir . 'pokemon-app-logs.txt';
+
+    $currentDateTime = date('Y-m-d H:i:s');
+    $userIP = $_SERVER['REMOTE_ADDR'];
+
+    $status = $success ? "Inscription réussie" : "Échec de l'inscription";
+
+    $logEntry = "/*              *\\\n" .
+               "----- " . $pseudo . " -----\n" .
+               "Date d'inscription : $currentDateTime\n" .
+               "IP : $userIP\n" .
+               "Statut : $status\n" . 
+               "----- [Register] -----\n\n";
+
+    file_put_contents($logFile, $logEntry, FILE_APPEND);
+    
+    error_log("Inscription utilisateur: $pseudo - Statut: $status");
+}
+
+function logLogout($pseudo) {
+    $logDir = getenv('HOME') . '/LogFiles/';
+    
+    if (!file_exists($logDir)) {
+        mkdir($logDir, 0755, true);
+    }
+    
+    $logFile = $logDir . 'pokemon-app-logs.txt';
+
+    $currentDateTime = date('Y-m-d H:i:s');
+    $userIP = $_SERVER['REMOTE_ADDR'];
+
+    $logEntry = "/*              *\\\n" .
+               "----- " . $pseudo . " -----\n" .
+               "Date de déconnexion : $currentDateTime\n" .
+               "IP : $userIP\n" .
+               "Statut : Déconnexion\n" . 
+               "----- [Logout] -----\n\n";
+
+    file_put_contents($logFile, $logEntry, FILE_APPEND);
+    
+    error_log("Déconnexion utilisateur: $pseudo");
+}
